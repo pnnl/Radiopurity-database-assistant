5c51a7e7142ebd44122caa3f1b309f2501aaf363
Elise Saxon <elise.saxon@pnnl.gov>
2020-06-12 17:58:40 -0700

    Added arg parser to toolkit, implemented initial $or functionality in append

--- python_mongo_toolkit.py
+++ python_mongo_toolkit.py
@@ -1,8 +1,10 @@
-import sys
-#import argparse
+import argparse
 import json
+import re
 from pymongo import MongoClient
 
+numeric_fields = ["measurements.results.value"]
+date_fields = ["measurements.date", "source.input.date"]
 valid_fields = ["grouping", "sample.name", "sample.description", "sample.source", "sample.id", "sample.owner.name", "sample.owner.contact", "measurements.results.isotope", "measurements.results.symbol", "measurements.results.unit", "measurements.results.value", "measurements.practitioner.name", "measurements.practitioner.contact", "measurements.technique","measurements.institution", "measurements.date", "measurements.description", "source.reference", "source.input.name", "source.input.contact", "source.input.date", "source.input.notes"]
 valid_str_comparisons = ["contains", "notcontains", "eq"]
 valid_num_comparisons = ["eq", "lt", "lte", "gt", "gte"]
@@ -22,25 +24,34 @@ def search(query):
     return resp
 
 # append to existing query
-def add_to_query(append_mode, field, comparison, value, existing_q={}):
+def add_to_query(field, comparison, value, existing_q={}, append_mode="ADD"):
     # validate arguments
     if field not in valid_fields:
         print("Error: the 'field' argument must be one of: "+', '.join(valid_fields))
-        return None
+        return existing_q
     if comparison not in set(valid_str_comparisons+valid_num_comparisons):
         print("Error: the 'comparison' argument must be one of: "+', '.join(set(valid_str_comparisons+valid_num_comparisons)))
-        return None
+        return existing_q
     if append_mode.upper() not in valid_appendmodes:
         print("Error: the 'append_mode' argument must be one of: "+', '.join(valid_appendmodes))
-        return None
+        return existing_q
+    if field in numeric_fields and type(value) not in [int, float]:
+        try:
+            value = float(value)
+        except:
+            print('Error: you must enter a numeric value when comparing fields in: '+ ', '.join(numeric_fields))
+            return existing_q
     if type(value) is str and comparison not in valid_str_comparisons:
         print("Error: when comparing string values, the comparison operator must be one of: "+', '.join(valid_str_comparisons))
-        return None
+        return existing_q
     if type(value) is not str and comparison not in valid_num_comparisons:
         print("Error: when comparing numeric values, the comparison operator must be one of: "+', '.join(valid_int_comparisons))
-        return None
+        return existing_q
 
     # define new term
+    if field in date_fields:
+        #TODO: implement date comparisons
+        pass
     if type(value) is str:
         if comparison == 'contains':
             search_val = {'$regex':'.*'+value+'.*'}
@@ -55,11 +66,15 @@ def add_to_query(append_mode, field, comparison, value, existing_q={}):
         new_term = {field:{comparison:value}}
 
     # add new term to existing_q
-    #TODO: figure out how to go about $or and $and functionality
-    existing_q[field] = new_term[field]
-    '''
+    #TODO: decide how to go about $or and $and functionality
+    existing_keys = list(existing_q.keys())
     if append_mode == 'OR':
-        print("HAVEN'T IMPLEMENTED $or FUNCTIONALITY YET.")
+        if '$or' in existing_keys:
+            # add to other $or elements (this groups all $or terms together))
+            existing_q['$or'].append(new_term)
+        else:
+            # creates an $or list out of this new term and the most recently added element (query dict --> "Python 3.6 onwards, the standard dict type maintains insertion order by default.")
+            existing_q['$or'] = [{existing_keys[-1]:existing_q.pop(existing_keys[-1])}, new_term]
     else:
         if field in existing_q.keys():
             print('A. creating $and from existing.')
@@ -70,7 +85,6 @@ def add_to_query(append_mode, field, comparison, value, existing_q={}):
         else:
             print('C. new element')
             existing_q[field] = new_term[field]
-    '''
 
     return existing_q
 
@@ -82,51 +96,33 @@ def insert(doc):
     pass
 
 if __name__ == '__main__':
-    func_keys = ['search','add','insert']
-    '''
     parser = argparse.ArgumentParser(description='A python toolkit for interfacing with the radiopurity_example MongoDB.')
-    parser.add_argument('--func', choices=['search','add','insert'], help='Specify which function to perform. \
-        "search" requires a dictionary query. "add" appends a field, comparison, value search term to an existing \
-        query. "insert" requires a single dictionary document in MADF format.')
-    args = parser.parse_args()
+    subparsers = parser.add_subparsers(help='options for which function to run', dest='subparser_name')
 
-    if args.func == 'search':
-        search()
-    elif args.func == 'add':
-        add_to_query()
-    elif args.func == 'insert':
-        insert()
-    '''
+    search_parser = subparsers.add_parser('search', help='execute search function')
+    search_parser.add_argument('--q', type=json.loads, required=True, help='query to execute. *must be surrounded with single quotes, and use double quotes within dict*')
 
-    if len(sys.argv) < 2:
-        print("Error: you must enter the function name to call, as well as all relevant arguments for that function.")
-        sys.exit()
-    if sys.argv[1] not in func_keys:
-        print("Error: the first argument must be one of the function keys: "+', '.join(func_keys))
-        sys.exit()
+    append_parser = subparsers.add_parser('add', help='execute append function (add new query term to query)')
+    append_parser.add_argument('--field', type=str, required=True, choices=valid_fields, help='the field to compare the value of')
+    append_parser.add_argument('--compare', type=str, required=True, choices=list(set(valid_str_comparisons+valid_num_comparisons)), \
+        help='comparison operator to use to compare actual field value to given value')
+    append_parser.add_argument('--val', type=str, required=True, help='the value to compare against. Can be a string or numeric')
+    append_parser.add_argument('--mode', type=str, choices=["OR", "AND"], default="AND", help='optional argument to define append mode. If not present, defaults to "AND"')
+    append_parser.add_argument('--q', type=json.loads, default={}, \
+        help='*must be surrounded with single quotes, and use double quotes within dict* existing query dictionary to add a new term to. If not present, creates a new query')
 
-    arg_len = len(sys.argv)
+    insert_parser = subparsers.add_parser('insert', help='execute document insert function')
+    insert_parser.add_argument('--doc', type=json.loads, required=True, \
+        help='The dictinoary document to insert into the database. *must be surrounded with single quotes, and use double quotes within dict*')
+    
+    args = vars(parser.parse_args())
 
-    if sys.argv[1] == 'search':
-        if arg_len != 3:
-            print("Error: when choosing 'search' you must enter a query dictionary.")
-            sys.exit()
-        result = search(json.loads(sys.argv[2]))
-    elif sys.argv[1] == 'add':
-        if arg_len == 6:
-            result = add_to_query(sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
-        elif arg_len == 7:
-            result = add_to_query(sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], json.loads(sys.argv[6]))
-        else:
-            print("Error: when choosing 'add' you must enter an append mode, field, comparison operator, value, and an optional existing query to add to.")
-            sys.exit()
-        result = json.dumps(result)
-    elif sys.argv[1] == 'insert':
-        if arg_len != 3:
-            print("Error: when choosing 'insert' you must enter a document dictionary in MADF format.")
-            sys.exit()
-        result = insert(json.loads(sys.argv[2]))
+    if args['subparser_name'] == 'search':
+        result = search(args['q'])
+    elif args['subparser_name'] == 'add':
+        result = add_to_query(args['field'], args['compare'], args['val'], existing_q=args['q'], append_mode=args['mode'])
+    elif args['subparser_name'] == 'insert':
+        result = insert(args['doc'])
 
-    #TODO: cannot return result?
     print(result)
 
