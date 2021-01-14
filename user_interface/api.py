31e40e14c4fdeb713049b19cf16129cdfa7663b9
Elise Saxon <elise.saxon@pnnl.gov>
2021-01-14 14:25:59 -0800

    Adding front-end components for assay request system. Uses existing back-end funcs without changes

--- user_interface/api.py
+++ user_interface/api.py
@@ -1,15 +1,12 @@
 import sys
-#import json
 import argparse
 import datetime
 from functools import wraps
 import scrypt
-from flask import Flask, request, session, url_for, redirect, render_template
+from flask import Flask, request, session, url_for, redirect, render_template, g
 from dunetoolkit import set_ui_db, search_by_id, convert_date_to_str
-#from python_mongo_toolkit import set_ui_db, search_by_id, convert_date_to_str
 from frontend_helpers import do_q_append, parse_existing_q, perform_search, perform_insert, parse_update, perform_update
 from frontend_helpers import _get_user
-#from query_class import Query
 
 app = Flask(__name__)
 sk = None
@@ -99,7 +96,7 @@ def restricted_page():
     return render_template('restricted_page.html')
 
 @app.route('/search', methods=['GET','POST'])
-#@requires_permissions(['DUNEreader', 'DUNEwriter', 'Admin'])
+@requires_permissions(['DUNEreader', 'DUNEwriter', 'Admin'])
 def search_endpoint():
     final_q_lines_list = []
     append_mode = ''
@@ -216,50 +213,142 @@ def update_endpoint():
     return None
 
 
+@app.route('/assay_request', methods=['GET','POST'])
+@requires_permissions(['DUNEreader', 'DUNEwriter',  'Admin'])
+def assay_request_endpoint():
+    if request.method == "POST":
+        new_doc_id, error_msg = perform_insert(request.form, coll_type='assay_requests')
+        new_doc_msg = 'assay request doc id: '+str(new_doc_id)
+        if new_doc_id is None:
+            new_doc_msg = "ERROR: record not inserted because "+error_msg
+    else:
+        new_doc_msg = ""
+    return render_template('assay_request.html', new_doc_msg=new_doc_msg)
+
+@app.route('/assay_request_search', methods=['GET','POST'])
+@requires_permissions(['DUNEwriter', 'Admin'])
+def assay_request_search_endpoint():
+    final_q_lines_list = []
+    append_mode = ''
+    results = []
+    results_str = []
+
+    if request.form.get("append_button") == "do_and":
+        print('and...')
+        q_dict, q_str, num_q_lines, error_msg = do_q_append(request.form)
+        append_mode = "AND"
+
+    elif request.form.get("append_button") == "do_or":
+        print('or...')
+        q_dict, q_str, num_q_lines, error_msg = do_q_append(request.form)
+        append_mode = "OR"
+
+    elif request.method == "POST":
+        print('search...')
+        final_q, final_q_str, num_q_lines, error_msg = do_q_append(request.form)
+
+        results, error_msg = perform_search(final_q, coll_type='assay_requests')
+
+        final_q_lines_list = []
+        if final_q_str != '':
+            final_q_lines_list = final_q_str.split('\n')
+
+        results_str = [ str(r) for r in results ]
+        q_dict = {}
+        q_str = ''
+        num_q_lines = 0
+
+    else:
+        q_dict = {}
+        q_str = ''
+        num_q_lines = 0
+        error_msg = ''
+
+    print('num q lines:',num_q_lines)
+    return render_template('assay_request_search.html', existing_query=q_str, append_mode=append_mode, error_msg=error_msg, num_q_lines=num_q_lines, final_q=final_q_lines_list, results_str=results_str, results_dict=results)
+
+@app.route('/assay_request_update', methods=['GET','POST'])
+@requires_permissions(['DUNEwriter', 'Admin'])
+def assay_request_update_endpoint():
+    if request.method == "GET":
+        return render_template('assay_request_update.html', doc_data=False, message="")
+
+    elif request.form.get("submit_button") == "find_doc":
+        doc_id = request.form.get('doc_id', '')
+        doc = search_by_id(doc_id, coll_type='assay_requests')
+        print('ASSAY REQUEST DOC:::',doc)
+
+        if doc is None:
+            return render_template('assay_request_update.html', doc_data=False, message="No assay request was found with the ID you provided.")
+
+        for i in range(len(doc['measurement']['results'])):
+            num_vals = len(doc['measurement']['results'][i]['value'])
+            if num_vals == 0:
+                doc['measurement']['results'][i]['value'] = []
+            if num_vals < 2:
+                doc['measurement']['results'][i]['value'].append('')
+            if num_vals < 3:
+                doc['measurement']['results'][i]['value'].append('')
+
+        return render_template('assay_request_update.html', doc_data=True, doc_id=doc['_id'], \
+                grouping=doc['grouping'], \
+                sample_name=doc['sample']['name'], \
+                sample_description=doc['sample']['description'], \
+                sample_source=doc['sample']['source'], \
+                sample_id=doc['sample']['id'], \
+                sample_owner_name=doc['sample']['owner']['name'], \
+                sample_owner_contact=doc['sample']['owner']['contact'], \
+                data_reference=doc['data_source']['reference'], \
+                data_input_name=doc['data_source']['input']['name'], \
+                data_input_contact=doc['data_source']['input']['contact'], \
+                data_input_date=' '.join([convert_date_to_str(date_ele) for date_ele in doc['data_source']['input']['date']]), \
+                data_input_notes=doc['data_source']['input']['notes'], \
+                measurement_practitioner_name=doc['measurement']['practitioner']['name'], \
+                measurement_practitioner_contact=doc['measurement']['practitioner']['contact'], \
+                measurement_technique=doc['measurement']['technique'], \
+                measurement_institution=doc['measurement']['institution'], \
+                measurement_date=' '.join([convert_date_to_str(date_ele) for date_ele in doc['measurement']['date']]), \
+                measurement_description=doc['measurement']['description'], \
+                measurement_requestor_name=doc['measurement']['requestor']['name'], \
+                measurement_requestor_contact=doc['measurement']['requestor']['contact'], \
+                measurement_results=doc['measurement']['results']
+            )
+
+    else:
+        if request.form.get("submit_button") == "update_doc":
+            action = 'update'
+            is_update = True
+            is_verify = False
+        elif request.form.get("submit_button") == "verify_doc":
+            action = 'verification'
+            is_update = False
+            is_verify = True
+
+        doc_id, remove_doc, update_pairs, meas_remove_indices, meas_add_eles = parse_update(request.form)
+        new_doc_id, error_msg = perform_update(doc_id, remove_doc, update_pairs, meas_remove_indices, meas_add_eles, is_assay_request_update=is_update, is_assay_request_verify=is_verify)
+        if new_doc_id != None:
+            message = "assay request "+action+" success. New doc version ID: "+str(new_doc_id)
+        else:
+            message = "assay request "+action+" error: "+error_msg
+        return render_template('assay_request_update.html', doc_data=False, message=message)
+
+    return None
+
 
 
 @app.before_first_request
 def _setup_database():
     global database_name
-    #database_name = 'dune'
-    #collection_name = 'dune_data'
-    database_name = 'radiopurity_data'
-    collection_name = 'testing'
-    port = 8001
-    successful_change = set_ui_db(database_name, collection_name)
-    if not successful_change:
-        print('error: unable to change mongodb to database:',database_name,'and collection:',collection_name)
-        sys.exit()
-    else:
-        print('using mongo database:',database_name)
-
-"""
-if __name__ == '__main__':
-    global database_name
-    '''
-    parser = argparse.ArgumentParser(description='API code for the DUNE project.')
-    parser.add_argument('--port', type=int, default=5000, help='the port number to run the UI on.')
-    parser.add_argument('--db', type=str, choices=['radiopurity', 'dune'], required=False, \
-        help='the type of data to use with the UI. The "radiopurity" option uses the database \
-        containing data extracted from the radiopurity site. The "dune" option uses the database \
-        for data from the DUNE project.')
-    args = parser.parse_args()
-
-    if args.db == 'dune':
-        print('Using dune database')
-        database_name = 'dune'
-        collection_name = 'dune_data'
-    elif args.db == 'radiopurity':
-        print('Using radiopurity database')
-        database_name = 'radiopurity_data'
-        collection_name = 'example_data'
-    else:
-        print('No database specified as argument; using default radiopurity testing database (radiopurity_data.testing).')
-        database_name = 'radiopurity_data'
-        collection_name = 'testing'
-    '''
+    
     database_name = 'dune'
     collection_name = 'dune_data'
+    
+    #database_name = 'radiopurity_data'
+    #collection_name = 'testing'
+    
+#    database_name = 'dune_pytest_data'
+#    collection_name = 'test_data'
+
     port = 8001
     successful_change = set_ui_db(database_name, collection_name)
     if not successful_change:
@@ -268,6 +357,21 @@ if __name__ == '__main__':
     else:
         print('using mongo database:',database_name)
 
-    app.run(host='127.0.0.1', port=port)
-"""
+'''
+@app.route('/change_db', methods=['GET','POST'])
+def change_db():
+    if request.method == 'POST':
+        db_name = request.form.get("database_name", "")
+        coll_name = request.form.get("collection_name", "")
+        database_name = db_name
+        collection_name = coll_name
+        successful_change = set_ui_db(database_name, collection_name)
+        if not successful_change:
+            print('error: unable to change mongodb to database:',database_name,'and collection:',collection_name)
+        else:
+            print('now using mongo database:',database_name)
+
+    return render_template('change_db.html')
+'''
+
 
