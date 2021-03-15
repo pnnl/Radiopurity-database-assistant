7a54ac85aefa1f0e722977040b99f62ff45b9d9a
Elise Saxon <elise.saxon@pnnl.gov>
2021-03-15 15:09:39 -0700

    Adding manifest file in order to include txt and csv files in package

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ setuptools.setup(
     author="Elise Saxon",
     author_email="elise.saxon@pnnl.gov",
     description="A package that helps collaborators use the DUNE radiopurity database assistant",
-    long_description='',
-    long_description_content_type="text/markdown",
     url="https://gitlab.pnnl.gov/saxo675/dune",
+    package_data={'dunetoolkit':['synonyms.txt', 'units.csv', 'isotopes.csv']},
     packages=setuptools.find_packages(),
     license="BSD",
-    python_requires='>=3.6',
+    python_requires='>=3.7',
+    include_package_data=True,
     install_requires=[
         'jsonschema==2.6.0',
         'pymongo'
