bc5bdb513635193810e75225f9cccabc04dcaa08
Elise Saxon <elise.saxon@pnnl.gov>
2021-05-21 12:43:34 -0700

    Adding a Dockerfile to create an image to run tests, also editing toolkit tests to use the toolkit test config

--- tests/test_python_toolkit_insert.py
+++ tests/test_python_toolkit_insert.py
@@ -243,11 +243,15 @@ def test_insert_bad_date_d():
     measurement_date=measurement_date)
     assert new_doc_id == None
 
-
+def get_mongodb_config_info():
+    with open(os.environ.get('TOOLKIT_CONFIG_NAME'), 'r') as rf:
+        config = json.load(rf)
+        return config['mongodb_host'], config['mongodb_port'], config['database']
 
 def set_up_db_for_test():
-    client = MongoClient('localhost', 27017)
-    db_obj = client.dune_pytest_data
+    db_host, db_port, db_name = get_mongodb_config_info()
+    client = MongoClient(db_host, db_port)
+    db_obj = client[db_name]
     coll = db_obj.assays
     coll.insert_one({ "_id" : ObjectId("000000000000000000000001"), "type" : "testing_doc" })
     coll.insert_one({ "_id" : ObjectId("000000000000000000000002"), "measurement" : { "description" : "", "practitioner" : { "name" : "ICI Tracerco", "contact" : "" }, "requestor" : { "name" : "", "contact" : "" }, "date" : [ ], "institution" : "", "technique" : "NAA", "results" : [ { "unit" : "ppb", "value" : [ 18, 2 ], "isotope" : "U-238", "type" : "measurement" }, { "unit" : "ppb", "value" : [ 59, 2 ], "isotope" : "Th-232", "type" : "measurement" }, { "unit" : "ppm", "value" : [ 0.78, 0.02 ], "isotope" : "K-40", "type" : "measurement" } ] }, "grouping" : "ILIAS UKDM", "specification" : "3.00", "data_source" : { "input" : { "date" : [ datetime.datetime(2013, 7, 22, 0, 0) ], "name" : "Ben Wise / James Loach", "contact" : "bwise@smu.edu / james.loach@gmail.com", "notes" : "" }, "reference" : "ILIAS Database http://radiopurity.in2p3.fr/" }, "sample" : { "description" : "Resin, Magnex, 2:1 Thiokol 308, RAL", "id" : "ILIAS UKDM #249", "owner" : { "name" : "", "contact" : "" }, "name" : "Resin, Magnex, 2:1 Thiokol 308", "source" : "" }, "type" : "measurement", "_version" : 1 })
@@ -258,9 +262,10 @@ def set_up_db_for_test():
     return db_obj
 
 def teardown_db_for_test():
-    client = MongoClient('localhost', 27017)
-    coll = client.dune_pytest_data.assays
-    old_versions_coll = client.dune_pytest_data.assays_old_versions
+    db_host, db_port, db_name = get_mongodb_config_info()
+    client = MongoClient(db_host, db_port)
+    coll = client[db_name].assays
+    old_versions_coll = client[db_name].assays_old_versions
     remove_resp = coll.delete_many({})
     resmove_oldversions_resp = old_versions_coll.delete_many({})
 
